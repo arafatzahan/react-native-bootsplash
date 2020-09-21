c0a9b8d5dc929796b487956b98fbb849c7493fd2
Arafat Zahan <arafatzahan.starit@gmail.com>
2020-09-21 14:38:17 +0600

    React Native Web Support

--- src/react-native/native-modules.ts
+++ src/react-native/native-modules.ts
@@ -1,19 +1,74 @@
-import { RNBootSplash } from "..";
+import { RNBootSplash, VisibilityStatus } from "..";
 
 type NMType = {
   RNBootSplash: RNBootSplash;
 };
 
+const sleep = (ms: number) => {
+  return new Promise((resolve) => setTimeout(resolve, ms));
+};
+
 const NativeModules: NMType = {
   RNBootSplash: {
     async show(fade) {
-      return await Promise.resolve(true);
+      const bootsplash = document.getElementById("bootsplash");
+      if (!bootsplash) {
+        console.warn("Bootsplash not found, have you set up your entrypoint?");
+        return true;
+      }
+      const visibilityStatus: VisibilityStatus =
+        (bootsplash.getAttribute("data-visibility") as VisibilityStatus) ||
+        "visible";
+
+      if (visibilityStatus === "hidden") {
+        if (fade) {
+          bootsplash.setAttribute("data-visibility", "transitioning");
+          bootsplash.className = "visibleAnim";
+          await sleep(600);
+          bootsplash.setAttribute("data-visibility", "visible");
+        } else {
+          bootsplash.className = "visible";
+          bootsplash.setAttribute("data-visibility", "visible");
+        }
+      }
+      return true;
     },
     async hide(fade) {
-      return await Promise.resolve(true);
+      const bootsplash = document.getElementById("bootsplash");
+      if (!bootsplash) {
+        console.warn("Bootsplash not found, have you set up your entrypoint?");
+        return true;
+      }
+      const visibilityStatus: VisibilityStatus =
+        (bootsplash.getAttribute("data-visibility") as VisibilityStatus) ||
+        "visible";
+
+      if (visibilityStatus === "visible") {
+        if (fade) {
+          bootsplash.setAttribute("data-visibility", "transitioning");
+          bootsplash.className = "hiddenAnim";
+          await sleep(600);
+          bootsplash.setAttribute("data-visibility", "hidden");
+        } else {
+          bootsplash.className = "hidden";
+          bootsplash.setAttribute("data-visibility", "hidden");
+        }
+      }
+      return true;
     },
     async getVisibilityStatus() {
-      return await Promise.resolve("transitioning");
+      const bootsplash = document.getElementById("bootsplash");
+      if (!bootsplash) {
+        console.warn("Bootsplash not found, have you set up your entrypoint?");
+        // Incomplete setup, no bootsplash is showing.
+        return "hidden";
+      }
+      const visibilityStatus = bootsplash.getAttribute("data-visibility");
+      if (!visibilityStatus) {
+        // First Run
+        return "visible";
+      }
+      return visibilityStatus as VisibilityStatus;
     },
   },
 };
